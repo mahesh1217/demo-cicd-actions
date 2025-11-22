d36fab64a9207638715819915cce47351b6bfd39
mahesh1217 <maheshkoppineedi7@gmail.com>
2025-11-23 00:18:05 +0530

    code

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	@GetMapping("/hi")
+    public String sayHi() {
+        return "Hi";
+    }
+	
+	@GetMapping("/message")
+	public Integer sumOff(){
+		int a = 2;
+		int b = 4;
+		int sum = a+b;
+		return sum;
+	}
+
+//    echo "# demo-cicd-actions" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/mahesh1217/demo-cicd-actions.git
+//    git push -u origin main
+
+}
